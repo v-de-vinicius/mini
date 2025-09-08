46c5fbe092a9f15e54afd11b0759084ee2145485
v-de-vinicius <vinicius.matosfelix@gmail.com>
2025-09-08 12:02:28 -0300

    feat: add rudimentary header parsing

--- src/main/java/com/github/vdevinicius/Main.java
+++ src/main/java/com/github/vdevinicius/Main.java
@@ -1,9 +1,13 @@
 package com.github.vdevinicius;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.SocketTimeoutException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class Main {
 
@@ -20,6 +24,22 @@ public class Main {
                     while (bodyIndex == -1) {
                         accumulator.write(buffer, 0, inputStream.read(buffer));
                         bodyIndex = getBodyIndex(accumulator.toByteArray(), accumulator.size());
+                    }
+                    final var headerBytes = accumulator.toByteArray();
+                    final var reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(headerBytes)));
+                    reader.readLine(); // discard first line — HTTP <METHOD> / 1.1
+                    final var headers = new HashMap<String, String>();
+                    var header = reader.readLine();
+                    while (!header.isEmpty()) {
+                        final var headerFragments = header.split(":");
+                        final var key = headerFragments[0].toLowerCase();
+                        final var value = headerFragments[1].trim().toLowerCase();
+                        if (headers.containsKey(key)) {
+                            headers.put(key, headers.get(key) + ";" + value);
+                        } else {
+                            headers.put(key, value);
+                        }
+                        header = reader.readLine();
                     }
                     System.out.println("[mini-http] Starting reading body in position [" + bodyIndex + "]");
                     outputStream.write("Request readed sucessfully.".getBytes(StandardCharsets.UTF_8));
