4d7ee5bbe1e9a3ff18dc436eac01fad63c3f7532
v-de-vinicius <vinicius.matosfelix@gmail.com>
2025-09-21 15:16:27 -0300

    feat: add special headers treatment + improvements on header reading and parsing

--- src/main/java/com/github/vdevinicius/Main.java
+++ src/main/java/com/github/vdevinicius/Main.java
@@ -5,8 +5,11 @@ import java.net.ServerSocket;
 import java.net.SocketTimeoutException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
 
 public class Main {
+    private static final int MAX_BODY_SIZE = 1024 * 1024;
 
     public static void main(String[] args) {
         try (final var serverSocket = new ServerSocket(8080)) {
@@ -27,21 +30,26 @@ public class Main {
                         headerAccumulator.write(buffer, 0, n);
 
                         if (headerAccumulator.size() > 32768) {
-                            throw new RuntimeException("Header size greater than 32KiB");
+                            // Write body back in the response
+                            outputStream.write("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n".getBytes(StandardCharsets.US_ASCII));
+                            outputStream.flush();
+                            return;
                         }
                         bodyIndex = getBodyIndex(headerAccumulator.toByteArray(), headerAccumulator.size());
                     }
                     final var requestBytes = headerAccumulator.toByteArray();
-                    final var headerReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(requestBytes), StandardCharsets.US_ASCII));
-                    headerReader.readLine(); // discard first line — HTTP <METHOD> / 1.1
+                    final var headerBytes = new ByteArrayInputStream(requestBytes, 0, bodyIndex);
+                    final var headerReader = new BufferedReader(new InputStreamReader(headerBytes, StandardCharsets.US_ASCII));
+                    headerReader.readLine(); // <METHOD> / HTTP/1.1
                     final var headers = new HashMap<String, String>();
                     var header = headerReader.readLine();
                     while (!header.isEmpty()) {
                         final var headerFragments = header.split(":", 2);
                         final var key = headerFragments[0].trim().toLowerCase();
                         final var value = headerFragments.length > 1 ? headerFragments[1].trim() : "";
-                        if (headers.containsKey(key)) {
-                            headers.put(key, headers.get(key) + ";" + value);
+                        // Set-Cookie header must be treated separately according to RFC 6265 - So we're supporting it as a single value header.
+                        if (headers.containsKey(key) && !key.equals("set-cookie")) {
+                            headers.put(key, headers.get(key) + "," + value);
                         } else {
                             headers.put(key, value);
                         }
@@ -51,25 +59,58 @@ public class Main {
                     // Body and content-length are relative to the request method - some methods might not contain it.
                     var contentLength = 0;
                     try {
-                        contentLength = Integer.parseInt(headers.get("content-length"));
+                        if (headers.containsKey("content-length")) {
+                            contentLength = Integer.parseInt(headers.get("content-length"));
+                        }
                     } catch (NumberFormatException e) {
                         throw new RuntimeException("Invalid content-length");
                     }
 
-                    final var body = new ByteArrayOutputStream();
+                    if (contentLength < 0) {
+                        outputStream.write("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n".getBytes(StandardCharsets.US_ASCII));
+                        outputStream.flush();
+                        // Return might end server?
+                        return;
+                    }
+
+                    if (!headers.containsKey("host")) {
+                        outputStream.write("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n".getBytes(StandardCharsets.US_ASCII));
+                        outputStream.flush();
+                        return;
+                    }
+
+                    if (headers.containsKey("transfer-encoding")) {
+                        outputStream.write("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n".getBytes(StandardCharsets.US_ASCII));
+                        outputStream.flush();
+                        return;
+                    }
+
+                    final var bodyAccumulator = new ByteArrayOutputStream();
                     final var already = headerAccumulator.size() - bodyIndex;
                     if (already > 0) {
-                        body.write(requestBytes, bodyIndex, already);
+                        bodyAccumulator.write(requestBytes, bodyIndex, already);
                     }
-                    var remaining = Integer.parseInt(headers.get("content-length")) - already;
+                    var remaining = contentLength - already;
                     while (remaining > 0) {
                         final var n = inputStream.read(buffer, 0, Math.min(buffer.length, remaining));
-                        if (n == -1) throw new RuntimeException("EOF before the end of body");
-                        body.write(buffer, 0, n);
+                        if (n == -1) {
+                            outputStream.write("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n".getBytes(StandardCharsets.US_ASCII));
+                            outputStream.flush();
+                            return;
+                        }
+                        bodyAccumulator.write(buffer, 0, n);
+
+                        if (bodyAccumulator.size() > MAX_BODY_SIZE) {
+                            outputStream.write("HTTP/1.1 413 Payload Too Large\r\nContent-Length: 0\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n".getBytes(StandardCharsets.US_ASCII));
+                            outputStream.flush();
+                            return;
+                        }
                         remaining -= n;
                     }
                     System.out.println("[mini-http] Starting reading body in position [" + bodyIndex + "]");
-                    outputStream.write("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\nConnection: close\r\n\r\n".getBytes(StandardCharsets.US_ASCII));
+                    // TODO: Finish response utils to improve response building
+                    final var response = ResponseUtils.getResponse(200, "Content-Length: 0", "Connection: close", "\r\n");
+                    outputStream.write(response.getBytes(StandardCharsets.US_ASCII));
                     outputStream.flush();
                 } catch (SocketTimeoutException e) {
                     System.out.println("[mini-http] Socket automatically closed after 5s of inactivity.");
@@ -90,5 +131,22 @@ public class Main {
         }
 
         return -1;
+    }
+
+    public static class ResponseUtils {
+        private static final Map<Integer, String> RESPONSE_STATUS_MAP = Map.of(
+                200, "OK",
+                400, "Bad Request",
+                413, "Payload Too Large"
+        );
+
+        public static String getResponse(int status, String... headers) {
+            final var joiner = new StringJoiner("\r\n");
+            joiner.add("HTTP/1.1 %d %s".formatted(status, RESPONSE_STATUS_MAP.get(status)));
+            for (final var header : headers) {
+                joiner.add(header);
+            }
+            return joiner.toString();
+        }
     }
 }
