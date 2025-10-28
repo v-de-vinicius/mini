caa4ac6f64ef039528ae404b05805e0887de159a
v-de-vinicius <vinicius.matosfelix@gmail.com>
2025-10-28 00:07:09 -0300

    test: add illegal body length test case

--- src/test/java/com/github/vdevinicius/minihttp/HttpMessageReaderTest.java
+++ src/test/java/com/github/vdevinicius/minihttp/HttpMessageReaderTest.java
@@ -33,12 +33,28 @@ public class HttpMessageReaderTest {
     }
 
     @Test
-    void whenHeaderSizeExceedsTheMaximumPermitted_thenShouldThrowIllegalStateException() {
+    void whenHeaderSizeExceedsTheMaximumPermitted_thenShouldThrowInvalidHttpMessageException() {
         final var joiner = new StringJoiner("\r\n");
         joiner.add("GET / HTTP/1.1");
         joiner.add("Host: 127.0.0.1");
         for (var i = 0; i < 33000; i++) {
             joiner.add("Mini-Header-%1$d: %1$d".formatted(i));
+        }
+        joiner.add("");
+        final var rawMessage = joiner.toString();
+        final var in = new ByteArrayInputStream(rawMessage.getBytes(StandardCharsets.UTF_8));
+        final var sut = new HttpMessageReader(in, BUFFER_SIZE);
+        assertThrows(InvalidHttpMessageException.class, sut::read);
+    }
+
+    @Test
+    void whenBodySizeExceedTheMaximumPermitted_thenShouldThrowInvalidHttpMessageException() {
+        final var joiner = new StringJoiner("\r\n");
+        joiner.add("GET / HTTP/1.1");
+        joiner.add("Host: 127.0.0.1");
+        joiner.add("");
+        for (var i = 0; i < 33000; i++) {
+            joiner.add("Mini-Body-%1$d: %1$d".formatted(i));
         }
         joiner.add("");
         final var rawMessage = joiner.toString();
